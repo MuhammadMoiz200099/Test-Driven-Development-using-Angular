fd77d013c70f3e02a68cc7cb76fb8b0266f0dcf4
Muhammad Moiz <muhammadmoiz0087@gmail.com>
2020-12-19 22:33:25 +0500

    Fixed tslib unmouting

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('td app is running!');
+    expect(page.getTitleText()).toEqual('tddproject app is running!');
   });
 
   afterEach(async () => {
