d14eee4d5190b771fcc9b8e55ff3734b0f499021
Muhammad Moiz <muhammadmoiz0087@gmail.com>
2020-12-19 23:45:50 +0500

    Added the routing Added the components Added the navigation Added the introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderNavigationComponent } from './components/header-navigation/header-navigation.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavigationComponent,
+    CalculatorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
